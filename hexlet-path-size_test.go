6f33e705710b7b031e5c9d045af6155294009cdb
Zhigulin Mark <zhigulinmark@gmail.com>
2026-01-18 22:15:17 +0300

    Перенес тест рядом с файлом

--- hexlet-path-size_test.go
+++ hexlet-path-size_test.go
@@ -1,7 +1,6 @@
-package tests
+package code
 
 import (
-	"code"
 	"os"
 	"testing"
 
@@ -11,68 +10,68 @@ import (
 func TestGetSize(t *testing.T) {
 	cwd, _ := os.Getwd()
 	fixturePath := cwd + "/testdata"
-	res, _ := code.GetPathSize(fixturePath, false, false, false)
+	res, _ := GetPathSize(fixturePath, false, false, false)
 	require.Equal(t, "1049625B", res)
 }
 
 func TestGetSizeDirectory(t *testing.T) {
 	cwd, _ := os.Getwd()
 	fixturePath := cwd + "/testdata/fixture_directory_1,2mb"
-	res, _ := code.GetPathSize(fixturePath, false, false, false)
+	res, _ := GetPathSize(fixturePath, false, false, false)
 	require.Equal(t, "1150974B", res)
 }
 
 func TestGetSizeFile(t *testing.T) {
 	cwd, _ := os.Getwd()
 	fixturePath := cwd + "/testdata/fixture_1kb.png"
-	res, _ := code.GetPathSize(fixturePath, false, false, false)
+	res, _ := GetPathSize(fixturePath, false, false, false)
 	require.Equal(t, "1049B", res)
 }
 
 func TestGetSizeError(t *testing.T) {
 	fixturePath := ""
-	_, err := code.GetPathSize(fixturePath, false, false, false)
+	_, err := GetPathSize(fixturePath, false, false, false)
 	require.EqualError(t, err, "empty path")
 }
 
 func TestHumanSize(t *testing.T) {
 	cwd, _ := os.Getwd()
 	fixturePath := cwd + "/testdata/fixture_1kb.png"
-	res, _ := code.GetPathSize(fixturePath, false, true, false)
+	res, _ := GetPathSize(fixturePath, false, true, false)
 	require.Equal(t, "1.0KB", res)
 }
 
 func TestWithoutHiddens(t *testing.T) {
 	cwd, _ := os.Getwd()
 	fixturePath := cwd + "/testdata/fixture_directory_with_hidden"
-	res, _ := code.GetPathSize(fixturePath, false, false, false)
+	res, _ := GetPathSize(fixturePath, false, false, false)
 	require.Equal(t, "1048575B", res)
 }
 
 func TestIncludeHiddens(t *testing.T) {
 	cwd, _ := os.Getwd()
 	fixturePath := cwd + "/testdata/fixture_directory_with_hidden"
-	res, _ := code.GetPathSize(fixturePath, false, false, true)
+	res, _ := GetPathSize(fixturePath, false, false, true)
 	require.Equal(t, "2097150B", res)
 }
 
 func TestWithoutRecursive(t *testing.T) {
 	cwd, _ := os.Getwd()
 	fixturePath := cwd + "/testdata"
-	res, _ := code.GetPathSize(fixturePath, false, false, false)
+	res, _ := GetPathSize(fixturePath, false, false, false)
 	require.Equal(t, "1049625B", res)
 }
 
 func TestWithRecursive(t *testing.T) {
 	cwd, _ := os.Getwd()
 	fixturePath := cwd + "/testdata"
-	res, _ := code.GetPathSize(fixturePath, true, false, false)
+	res, _ := GetPathSize(fixturePath, true, false, false)
 	require.Equal(t, "3249192B", res)
 }
 
 func TestFolderWithdNested(t *testing.T) {
 	cwd, _ := os.Getwd()
 	fixturePath := cwd + "/testdata/fixture_with_hidden_and_nested"
-	res, _ := code.GetPathSize(fixturePath, true, false, false)
+	res, _ := GetPathSize(fixturePath, true, false, false)
 	require.Equal(t, "18B", res)
 }
